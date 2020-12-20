eb72c004f2766348800d5dba5e0572415ae41f16
Huszar Beata <beata.huszar@adevinta.com>
2020-12-20 18:12:52 +0100

    Amend mask logicto stretch strings

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,37 @@
 import xmasmusic from '../media/Background_Music_Box_Music.mp3';
 
 let active = false;
+let mask;
+let maskInitialTop;
+let maskInitialBottom;
+let maskIntialCenter;
+let maskContainer;
+let maskContainerIninitalHeight;
+let body;
+let woman;
 const music = new Audio(xmasmusic);
 
 window.onload = () => {
-  const mask = document.getElementById('mask');
-  const body = document.querySelector('body');
-  const woman = document.getElementById('santa-woman');
+  mask = document.getElementById('mask');
+  maskInitialTop = mask.getBoundingClientRect().top;
+  maskInitialBottom = mask.getBoundingClientRect().bottom;
+  maskIntialCenter = maskInitialTop + (maskInitialBottom - maskInitialTop) / 2;
+
+  maskContainer = document.getElementById('mask-container');
+  maskContainerIninitalHeight = maskContainer.getBoundingClientRect().height;
+
+  body = document.querySelector('body');
+  woman = document.getElementById('santa-woman');
 
   mask.addEventListener('transitionend', maskAnimationOver);
-  mask.addEventListener('mousedown', dragStart);
-  mask.addEventListener('mousemove', dragMove);
-  body.addEventListener('mouseup', dragOver);
 
+  mask.addEventListener('mousedown', dragStart);
   mask.addEventListener('touchstart', dragStart);
-  mask.addEventListener('touchmove', dragMove);
+
+  body.addEventListener('mousemove', dragMove);
+  body.addEventListener('touchmove', dragMove);
+
+  body.addEventListener('mouseup', dragOver);
   body.addEventListener('touchend', dragOver);
 
   woman.style.opacity = '1';
@@ -22,7 +39,7 @@ window.onload = () => {
 };
 
 function maskAnimationOver(e) {
-  music.pause();
+  // music.pause();
 }
 
 function dragStart(e) {
@@ -30,7 +47,7 @@ function dragStart(e) {
   if (mask.style.transition !== '') {
     mask.style.transition = '';
   }
-  music.play();
+  // music.play();
 }
 
 function dragMove(e) {
@@ -39,21 +56,28 @@ function dragMove(e) {
     xmasText.style.opacity = '1';
     xmasText.style.transition = 'opacity 2s ease-in';
     if (e.type === 'touchmove') {
-      moveMask(e.currentTarget, e.touches[0].clientY);
+      moveMask(e.touches[0].clientY);
     } else {
-      moveMask(e.currentTarget, e.clientY);
+      moveMask(e.clientY);
     }
   }
 }
 
 function dragOver(e) {
   active = false;
-  mask.style.transform = `translate(3px, calc(65vh * 0.43))`;
-  mask.style.transition = '3s ease-in';
+  // mask.style.transition = '3s ease-in';
 }
 
-function moveMask(mask, actionY) {
-  mask.style.transform = `translate(3px, calc(${
-    (actionY / window.innerHeight) * 100
-  }vh * 0.43))`;
+function moveMask(cursorY) {
+  const maskTop = mask.getBoundingClientRect().top;
+  const maskBottom = mask.getBoundingClientRect().bottom;
+  const positionDiff = cursorY - maskIntialCenter;
+  const wouldShrinkOriginalSize =
+    positionDiff < 0 && maskInitialTop === maskTop;
+
+  if (!wouldShrinkOriginalSize) {
+    maskContainer.style.height = `${
+      maskContainerIninitalHeight + positionDiff
+    }px`;
+  }
 }
